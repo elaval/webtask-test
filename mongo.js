d05e713ccfebba5b6a90d0ee3205ecf6658ca7f0
Ernesto Laval <ernesto.laval@gmail.com>
2017-08-01 06:11:18 +0100

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -11,25 +11,17 @@ var waterfall   = require('async').waterfall;
  */
 module.exports = function(ctx, cb) {
 
-    var MONGO_URL = ctx.data.MONGO_URL;
-    if (!MONGO_URL) return cb(new Error('MONGO_URL secret is missing'))
+    var MONGO_URL = ctx.data.MONGO_URL;    
+    MongoClient.connect(MONGO_URL, function(err, db) {
+      if(err) cb(err, null);
+  
+      db
+        .collection('summary_2009_2017')
+        .find({})
+        .toArray((err, result) => {
+            if(err) cb(err, null);
+            cb(null,result)
+        });
 
-    waterfall([
-        function connect_to_db(done) {
-            MongoClient.connect(MONGO_URL, function(err, db) {
-                if(err) return done(err);
-
-                done(null, db);
-            });
-      },
-      function do_something(db, done) {
-          db
-              .collection('summary_2009_2017')
-              .find({}, function (err, result) {
-                  if(err) return done(err);
-
-                  done(null, result);
-              });
-      }
-    ], cb);
+    });
 };
